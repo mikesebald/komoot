491ab2910e539f5b5f04c8c333a491812d2db450
mikesebald <michael.sebald@gmail.com>
2018-01-01 21:10:42 +0100

    distance calculation fixed

--- komoot.R
+++ komoot.R
@@ -1,6 +1,7 @@
 library(XML)
 library(lubridate)
-library(raster)
+library(sp)
+library(OpenStreetMap)
 
 infile <- htmlTreeParse("../komoot_data/2017-10-31_24717322_fahrradtour_export.gpx", 
                         useInternalNodes = TRUE)
@@ -24,19 +25,26 @@ df$elevationdiff <- as.numeric(df$elevation) - as.numeric(df$elevation2)
 df$lat2 <- c(0, head(df$lat, n = -1))
 df$lon2 <- c(0, head(df$lon, n = -1))
 
-
 df$dist <- apply(df, 1, function(x) {
-  pointDistance(c(as.numeric(x["lat2"]), as.numeric(x["lon2"])),
-                c(as.numeric(x["lat"]), as.numeric(x["lon"])),
-                lonlat = TRUE)
-  })
+  spDistsN1(matrix(c(as.numeric(x["lon2"]), as.numeric(x["lat2"])), ncol = 2),
+                c(as.numeric(x["lon"]), as.numeric(x["lat"])),
+                longlat = TRUE)
+})
+
 df$dist[1] <- 0
+dist <- sum(df$dist)
+dist
 
 max(df$elevation)
 min(df$elevation)
-dist <- sum(df$dist) / 1000
+# calculating the elevation this way is incorrect - too much noise
 sum(df[df$elevationdiff > 0, "elevationdiff"], na.rm = TRUE)
 sum(df[df$elevationdiff < 0, "elevationdiff"], na.rm = TRUE)
 time <- as.numeric(difftime(df$time[nrow(df)], df$time[1], units = "hours"))
 speed <- dist / time
+speed
 
+# to be done: plotting into map
+map <- openmap(as.numeric(c(max(df$lat), min(df$lon))),
+               as.numeric(c(min(df$lat), max(df$lon))), type = "osm")
+plot(map)
